9c2ae29927a7a2692cb3530b4fbf3bb4b18a7906
Zac Sanders <37851511+endurain@users.noreply.github.com>
2019-04-04 21:01:58 -0600

    Add files via upload

--- promotions.php
+++ promotions.php
@@ -9,35 +9,53 @@
  ?>
 
 <style>
+.specials-wrapper h3 {
+	text-transform: capitalize;
+	font-weight: 500;
+}
 .coupon-fullwidth img {
 	height:100%;
 	width:100%;
 }
 .coupon-fullwidth {
 	display: inline-flex;
-	border: 2px dashed #0082ca;
+	border: 2px dashed rgba(10, 106, 219, 1);
 	width: 100%;
 	margin: 1em 0;
 	height: 435px;
 }
-.coupon-fullwidth hr {
-	background-color: rgba(0, 85, 166, 1);
+
+.coupon-fullwidth hr,
+.sm-coupon-content hr {
+	background-color: rgba(10, 106, 219, 1);
 	height: 2px;
 	width: 124px;
 	margin-left: auto;
 	margin-right: auto;
 }
-.coupon-col-2, .coupon-col-1 {
+.coupon-col-2, .coupon-col-1, .bot-col-1,
+	.bot-col-2 {
 	width: 50%;
 }
 .coupon-content {
 	text-align: center;
   padding: 3em 6em 0;
 }
+.sm-coupon-content {
+	width: 100%;
+	text-align: center;
+	border: 2px dashed #0082ca;
+  margin-top: 2em;
+}
+.sm-coupon-wrapper {
+	display: flex;
+	flex-flow: wrap;
+}
 .coupon-content p {
 	margin:0;
 }
-.coupon-fullwidth h1, .coupon-fullwidth h2 {
+.coupon-fullwidth h1, .coupon-fullwidth h2,
+ 	.sm-coupon-content h1, .sm-coupon-content h2 {
 	color: rgba(10, 106, 219, 1);
 }
 .coupon-fullwidth h1 {
@@ -53,25 +71,73 @@
 	align-items: flex-end;
 	margin: 8em 1em 0;
 }
+.sm-coupon-bottom {
+	display: flex;
+  align-items: flex-end;
+  margin: 0em 1em 0;
+  justify-content: space-between;
+}
 .fw-coupon-bottom img {
 	height: 57px;
 	width: auto;
+}
+.sm-coupon-bottom img {
+  height: 39px;
+  width: auto;
 }
 .specials-bottom {
 	display: inline-flex;
 	text-align: center;
 	margin-top: 12px;
 }
-.fw-coupon-bottom p {
+.fw-coupon-bottom p,
+	.sm-coupon-bottom p {
 	font-size: 9px;
 	line-height: 11px;
 	text-align: left;
 	padding: 0 2em;
 }
+.sm-coupon-spacer {
+	width: 50%;
+	padding: 0 2em 0 0;
+}
+.specials-bottom .bot-col-2 {
+	background: -moz-linear-gradient(left, rgba(30,87,153,1) 49%,
+		rgba(32,124,202,1) 68%, rgba(32,124,202,1) 68%, rgba(32,124,202,1) 68%,
+		rgba(32,124,202,1) 68%, rgba(41,137,216,1) 100%, rgba(125,185,232,1) 100%); /* FF3.6-15 */
+	background: -webkit-linear-gradient(left, rgba(30,87,153,1) 49%,
+		rgba(32,124,202,1) 68%,rgba(32,124,202,1) 68%,rgba(32,124,202,1) 68%,
+		rgba(32,124,202,1) 68%,rgba(41,137,216,1) 100%,rgba(125,185,232,1) 100%); /* Chrome10-25,Safari5.1-6 */
+	background: linear-gradient(to right, rgba(30,87,153,1) 49%,
+		rgba(32,124,202,1) 68%,rgba(32,124,202,1) 68%,rgba(32,124,202,1) 68%,
+		rgba(32,124,202,1) 68%,rgba(41,137,216,1) 100%,rgba(125,185,232,1) 100%);
+}
+.specials-bottom h1, .specials-bottom h3, .specials-bottom h4 {
+	color: #fff;
+	text-transform: none;
+}
+.spec-bottom-text {
+	padding: 3em 11em;
+}
+/* balances small coupons in their container */
+.sm-coupon-spacer:nth-child(even) {
+	padding-right: unset;
+}
 @media only screen and (max-width: 768px) {
+	.spec-bottom-text {
+    padding: 4em 5em;
+}
+	.sm-coupon-spacer {
+    width: unset;
+    padding: unset;
+}
 	.fw-coupon-bottom {
     margin: auto;
     text-align: center;
+	}
+	.fw-coupon-bottom p,
+	 .sm-coupon-bottom p {
+		text-align: center;
 	}
 	.coupon-fullwidth img {
 		height: unset;
@@ -86,10 +152,12 @@
 	.fw-coupon-bottom img {
 		width:55%!important;
 	}
-	.fw-coupon-bottom, .specials-bottom {
+	.fw-coupon-bottom, .specials-bottom,
+	 	.sm-coupon-wrapper, .sm-coupon-bottom {
     display: block!important;
 	}
-	.coupon-col-1, .coupon-col-2 {
+	.coupon-col-1, .coupon-col-2, .sm-coupon-content,
+		.bot-col-1, .bot-col-2 {
 		width:100%!important;
 	}
 	.coupon-fullwidth {
@@ -109,59 +177,91 @@
 		<?php include(TEMPLATEPATH . '/page-interior-header.php'); ?>
 		<div id="content-wrapper">
 			<div class="container">
-			<div class="row">
-			  <div class="specials-wrapper">
-			    <div class"fw-coupon-top">
-			      <h1>Specials</h1>
-			      <p>some text will go here</p>
-			    </div>
-			    <div class="fw-coupon-wrapper">
-						<?php
-							if( have_rows('promotions_coupon_fullwidth') ): ?>
-							<?php while ( have_rows('promotions_coupon_fullwidth') ) : the_row(); ?>
-								<div class="coupon-fullwidth">
-									<div class="coupon-col-1">
-				          	<div class="coupon-content">
-											<h1><?php the_sub_field('coupon_h1_text'); ?></h1>
-											<h3><?php the_sub_field('coupon_h3_text'); ?></h3>
-												<hr>
-											<p><?php the_sub_field('estimate_text'); ?></p>
-											<br>
-											<p><?php the_sub_field('coupon_tag_line'); ?></p>
-											<h2><?php echo $phone ?></h2>
+				<div class="row">
+					<div class="col-md-8">
+					<?php include(TEMPLATEPATH . '/breadcrumbs.php'); ?>
+					 <section id="content" role="main">
+						 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+							<?php the_content(); ?>
+								<?php endwhile; else: ?>
+								<h2>Whoops...</h2>
+								<p>Sorry, no posts were found.</p>
+						 <?php endif; ?>
+					 </section><!-- end #content -->
+					</div><!--end.col-md-8-->
+				  <div class="specials-wrapper">
+				    <div class="fw-coupon-wrapper">
+							<?php
+								if( have_rows('promotions_coupon_fullwidth') ): ?>
+								<?php while ( have_rows('promotions_coupon_fullwidth') ) : the_row(); ?>
+									<div class="coupon-fullwidth">
+										<div class="coupon-col-1">
+					          	<div class="coupon-content">
+												<h1><?php the_sub_field('coupon_h1_text'); ?></h1>
+												<h3><?php the_sub_field('coupon_h3_text'); ?></h3>
+													<hr>
+												<p><?php the_sub_field('estimate_text'); ?></p>
+												<br>
+												<p><?php the_sub_field('coupon_tag_line'); ?></p>
+												<h2><?php echo $phone ?></h2>
+											</div>
+											<div class="fw-coupon-bottom">
+					              <img src="<?php echo $logo ?>" />
+					              <p><?php the_sub_field('expiration_text');?></p>
+					            </div>
 										</div>
-										<div class="fw-coupon-bottom">
-				              <img src="<?php echo $logo ?>" />
-				              <p><?php the_sub_field('expiration_text');?></p>
-				            </div>
+										<div class="coupon-col-2">
+						          <img src="<?php the_sub_field('coupon_image'); ?>" />
+						        </div>
 									</div>
-									<div class="coupon-col-2">
-					          <img src="<?php the_sub_field('coupon_image'); ?>" />
-					        </div>
-								</div>
+									<?php
+								endwhile;
+								endif; ?>
+				    </div>
+						<div class="sm-coupon-wrapper">
+							<?php if( have_rows('promotions_coupon_small') ): ?>
+								<?php while ( have_rows('promotions_coupon_small') ) : the_row(); ?>
+									<div class="sm-coupon-spacer">
+										<div class="sm-coupon-content">
+											<h1><?php the_sub_field('sm_coupon_h1_text'); ?></h1>
+											<h3><?php the_sub_field('sm_coupon_h3_text'); ?></h3>
+											<p><?php the_sub_field('sm_coupon_tag_line'); ?></p>
+												<hr>
+											<p>Call Us Today!</p>
+											<h2><?php echo $phone ?></h2>
+											<div class="sm-coupon-bottom">
+												<img src="<?php echo $logo ?>" />
+												<p><?php the_sub_field('sm_expiration_text');?></p>
+											</div>
+										</div>
+									</div>
 								<?php
 							endwhile;
 							endif; ?>
-			    </div>
-					<div class="sm-coupon-wrapper">
-						<?php if( have_rows('promotions_coupon_small') ): ?>
-							<?php while ( have_rows('promotions_coupon_small') ) : the_row(); ?>
-								<div class="sm-coupon-content">
-									<h1><?php the_sub_field('sm_coupon_h1_text'); ?></h1>
-									<h3><?php the_sub_field('sm_coupon_h3_text'); ?></h3>
-										<hr>
-									<p>Call Us Today!</p>
-									<h2><?php echo $phone ?></h2>
+						</div>
+						<div class="specials-bottom-wrapper">
+							<div class="specials-bottom">
+								<div class="bot-col-1">
+									<img src="<?php the_field('promotions_bottom_page'); ?>" />
 								</div>
-								<div class="fw-coupon-bottom">
-									<img src="<?php echo $logo ?>" />
-									<p><?php the_sub_field('expiration_text');?></p>
+								<div class="bot-col-2">
+									<div class="spec-bottom-text">
+										<h1>We're ready to help!</h1>
+										<h4>Call us now and mention any of these offers for instant savings!</h4>
+										<div class="click-to-call">
+											<a style="text-decoration: none;" href="tel:19737288900">
+												<h3>
+													<img src="/wp-content/uploads/2018/12/icon_phone_white.png" />
+												 		<?php echo $phone ?>
+											  </h3>
+										  </a>
+										</div>
+									</div>
 								</div>
-							<?php
-						endwhile;
-						endif; ?>
+								</div>
+							</div>
+						</div>
 					</div>
-				</div>
-		</div>
+			</div>
 		</div>
 <?php get_footer(); ?>
